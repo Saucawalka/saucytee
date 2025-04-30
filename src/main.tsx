5607bf4f9b73673a6a7ce5e175c5828d6eec93aa
Saucawalka <164861125+Saucawalka@users.noreply.github.com>
2025-04-30 22:10:33 +0100

    version1

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider  } from './components/Authcontext.tsx'
+import { AuthProvider  } from './components/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
